3a3b1cd816c6f68b1649098e8bd7c7e4150d1abb
Vytenis Bivainis <monrealis@gmail.com>
2017-02-19 23:24:27 +0200

    Use static imports

--- src/test/java/eu/vytenis/hamcrest/sqlmatchers/SqlMatcherTest.java
+++ src/test/java/eu/vytenis/hamcrest/sqlmatchers/SqlMatcherTest.java
@@ -1,5 +1,11 @@
 package eu.vytenis.hamcrest.sqlmatchers;
 
+import static eu.vytenis.hamcrest.sqlmatchers.ExpectedResult.Fail;
+import static eu.vytenis.hamcrest.sqlmatchers.ExpectedResult.Pass;
+import static eu.vytenis.hamcrest.sqlmatchers.MatcherType.Delete;
+import static eu.vytenis.hamcrest.sqlmatchers.MatcherType.Insert;
+import static eu.vytenis.hamcrest.sqlmatchers.MatcherType.Select;
+import static eu.vytenis.hamcrest.sqlmatchers.MatcherType.Update;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
@@ -25,14 +31,14 @@ public class SqlMatcherTest {
 	@Parameters
 	public static List<Object[]> parameters() {
 		List<Object[]> r = new ArrayList<Object[]>();
-		r.add(new Object[] { "select * from dual", MatcherType.Select, ExpectedResult.Pass });
-		r.add(new Object[] { "select *, from dual", MatcherType.Select, ExpectedResult.Fail });
-		r.add(new Object[] { "insert into mytable values(1)", MatcherType.Insert, ExpectedResult.Pass });
-		r.add(new Object[] { "insert into table", MatcherType.Insert, ExpectedResult.Fail });
-		r.add(new Object[] { "update mytable set a = b", MatcherType.Update, ExpectedResult.Pass });
-		r.add(new Object[] { "update mytable", MatcherType.Update, ExpectedResult.Fail });
-		r.add(new Object[] { "delete from mytable", MatcherType.Delete, ExpectedResult.Pass });
-		r.add(new Object[] { "delete from mytable where", MatcherType.Delete, ExpectedResult.Fail });
+		r.add(new Object[] { "select * from dual", Select, Pass });
+		r.add(new Object[] { "select *, from dual", Select, Fail });
+		r.add(new Object[] { "insert into mytable values(1)", Insert, Pass });
+		r.add(new Object[] { "insert into table", Insert, ExpectedResult.Fail });
+		r.add(new Object[] { "update mytable set a = b", Update, Pass });
+		r.add(new Object[] { "update mytable", Update, Fail });
+		r.add(new Object[] { "delete from mytable", Delete, Pass });
+		r.add(new Object[] { "delete from mytable where", Delete, Fail });
 		return r;
 	}
 
