646dda4961c2d3915f7f2c454eb746644545a3d0
Vytenis Bivainis <monrealis@gmail.com>
2017-02-19 23:48:59 +0200

    Temporarily refactor SqlMatcher

--- src/main/java/eu/vytenis/hamcrest/sqlmatchers/SqlMatcher.java
+++ src/main/java/eu/vytenis/hamcrest/sqlmatchers/SqlMatcher.java
@@ -1,6 +1,7 @@
 package eu.vytenis.hamcrest.sqlmatchers;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 import java.util.Vector;
 
 import org.gibello.zql.ParseException;
@@ -28,24 +29,49 @@ class SqlMatcher extends TypeSafeMatcher<String> {
 
 	@Override
 	protected boolean matchesSafely(String sqlFragment) {
+		List<?> statements;
 		try {
-			parseQuery(createFullSqlStatement(sqlFragment));
-			return true;
+			statements = parseStatements(createFullSqlStatement(sqlFragment));
 		} catch (ParseException e) {
 			return false;
+		}
+		ZStatement statement;
+		try {
+			statement = getExactlyOneStatement(statements);
+		} catch (NotOneStatement e) {
+			return false;
+		}
+		try {
+			ensureTypeMatchesExpected(statement);
+			return true;
 		} catch (ClassCastException e) {
 			return false;
 		}
+
 	}
 
 	private String createFullSqlStatement(String sqlFragment) {
 		return statementPrefix + sqlFragment + ";";
 	}
 
-	private void parseQuery(String statement) throws ParseException {
+	private List<?> parseStatements(String statement) throws ParseException {
 		ZqlParser parser = new ZqlParser();
 		parser.initParser(new ByteArrayInputStream(statement.getBytes()));
 		Vector<?> statements = parser.readStatements();
-		expectedType.cast(statements.get(0));
+		return statements;
+	}
+
+	private ZStatement getExactlyOneStatement(List<?> statements) throws NotOneStatement {
+		if (statements.size() != 1)
+			throw new NotOneStatement();
+		return (ZStatement) statements.iterator().next();
+	}
+
+	private void ensureTypeMatchesExpected(ZStatement statement) {
+		expectedType.cast(statement);
+	}
+
+	private static class NotOneStatement extends Exception {
+		private static final long serialVersionUID = 1L;
 	}
 }
